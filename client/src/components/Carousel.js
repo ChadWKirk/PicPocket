00817e2cd06bd5738eb972268e98d5037bd70d04
Chad Kirk <ChadWKirk@gmail.com>
2022-09-25 11:04:10 -0500

    carousel overlay banner

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -67,64 +67,67 @@ const Carousel2 = () => {
   }, []);
 
   return (
-    <Carousel
-      pause={false}
-      activeIndex={index}
-      onSelect={handleSelect}
-      className="carousel"
-    >
-      <Carousel.Item>
-        <img className="d-block w-100" src={slide1} alt="First slide" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide2}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide3}
-          alt="Third slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide4}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide5}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide6}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          height={400}
-          src={slide7}
-          alt="First slide"
-        />
-      </Carousel.Item>
-    </Carousel>
+    <div className="carouselContainer">
+      <div className="carouselText">Sell your images for free</div>
+      <Carousel
+        pause={false}
+        activeIndex={index}
+        onSelect={handleSelect}
+        className="carousel"
+      >
+        <Carousel.Item>
+          <img className="d-block w-100" src={slide1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide2}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide4}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide5}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide6}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            height={400}
+            src={slide7}
+            alt="First slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 
