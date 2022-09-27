950c896aa1b6fb4bb093436f6a1cbac66787acfd
Chad Kirk <ChadWKirk@gmail.com>
2022-09-27 12:26:31 -0500

    delete button works!

--- client/src/pages/LikeTestPage.js
+++ client/src/pages/LikeTestPage.js
@@ -3,6 +3,7 @@ import { React, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan as farTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 const LikeTestPage = () => {
   //give an array to map over
@@ -14,7 +15,7 @@ const LikeTestPage = () => {
     var count = -1;
     //set up an attribute to see if each button is acting individually
     var liked;
-
+    //attributes[4] is className
     function changeLikeStatus(e, liked) {
       if (
         e.target.farthestViewportElement.attributes[4].value ==
@@ -26,7 +27,16 @@ const LikeTestPage = () => {
         e.target.farthestViewportElement.attributes[4].value =
           "svg-inline--fa fa-heart liked invis";
       }
-      console.log("clicked");
+      console.log("like");
+    }
+    //put this on entire button so you can remove entire button if clicking trash can
+    function deleteFunction(e) {
+      if (
+        e.target.nearestViewportElement.attributes[4].value ==
+        "svg-inline--fa fa-trash-can delete"
+      ) {
+        e.target.ownerDocument.activeElement.className = "gone";
+      }
     }
     setMapArr(
       startArr.map((element) => {
@@ -35,17 +45,27 @@ const LikeTestPage = () => {
 
         return (
           <div key={count}>
-            <button className="emptyButton">
+            <button
+              className="emptyButton"
+              onClick={(e, className) => deleteFunction(e, className)}
+            >
               <FontAwesomeIcon
                 liked="false"
                 className="default"
                 icon={farHeart}
               />
+
               <FontAwesomeIcon
                 liked="true"
                 className="liked invis"
                 onClick={(e, liked) => changeLikeStatus(e, liked)}
                 icon={faHeart}
+              />
+
+              <FontAwesomeIcon
+                liked="del"
+                className="delete"
+                icon={farTrashCan}
               />
             </button>
           </div>
