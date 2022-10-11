24f739dcc45327147532e7536de08f57450c9464
Chad Kirk <ChadWKirk@gmail.com>
2022-10-11 18:51:42 -0500

    like test success!!

--- client/src/pages/MyLikes/LikeTestPage.js
+++ client/src/pages/MyLikes/LikeTestPage.js
@@ -1,80 +1,55 @@
 import { React, useEffect, useState } from "react";
 //font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faTrashCan as farTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 const LikeTestPage = () => {
-  //give an array to map over
-  const startArr = [1, 2];
-  //map results array
-  const [mapArr, setMapArr] = useState();
+  const [imgArr, setImgArr] = useState([]);
+  const [fetchArr, setFetchArr] = useState([]);
+  const [like, setLike] = useState(false);
+
+  var mapArr;
+
   useEffect(() => {
-    //set up counter to represent index of item in map arr
-    var count = -1;
-    //set up an attribute to see if each button is acting individually
-    var liked;
-    //attributes[4] is className
-    function changeLikeStatus(e, liked) {
-      if (
-        e.target.farthestViewportElement.attributes[4].value ==
-        "svg-inline--fa fa-heart liked invis"
-      ) {
-        e.target.farthestViewportElement.attributes[4].value =
-          "svg-inline--fa fa-heart liked";
-      } else {
-        e.target.farthestViewportElement.attributes[4].value =
-          "svg-inline--fa fa-heart liked invis";
-      }
-      console.log("like");
-    }
-    //put this on entire button so you can remove entire button if clicking trash can
-    function deleteFunction(e) {
-      if (
-        e.target.nearestViewportElement.attributes[4].value ==
-        "svg-inline--fa fa-trash-can delete"
-      ) {
-        e.target.ownerDocument.activeElement.className = "gone";
-      }
-    }
-    setMapArr(
-      startArr.map((element) => {
-        //increment each of these per element of map array
-        count = count + 1;
-
-        return (
-          <div key={count}>
-            <button
-              className="emptyButton"
-              onClick={(e, className) => deleteFunction(e, className)}
-            >
-              <FontAwesomeIcon
-                liked="false"
-                className="default"
-                icon={farHeart}
-              />
-
-              <FontAwesomeIcon
-                liked="true"
-                className="liked invis"
-                onClick={(e, liked) => changeLikeStatus(e, liked)}
-                icon={faHeart}
-              />
-
-              <FontAwesomeIcon
-                liked="del"
-                className="delete"
-                icon={farTrashCan}
-              />
-            </button>
-          </div>
-        );
-      })
-    );
+    setFetchArr([
+      { url: "url", likedBy: ["chad"] },
+      { url: "url", likedBy: [] },
+      { url: "url", likedBy: ["lisa"] },
+    ]);
   }, []);
 
-  return <div>{mapArr}</div>;
+  useEffect(() => {
+    mapArr = fetchArr.map((element, index) => {
+      return (
+        <button
+          onClick={() => handleLike(element, index)}
+          className={`${element.likedBy.includes("chad") ? "blue" : ""}`}
+        >
+          Like
+        </button>
+      );
+    });
+
+    setImgArr(mapArr);
+  }, [fetchArr, like]);
+
+  function handleLike(element, index) {
+    setLike(!like);
+    var fetchArrCopy = fetchArr;
+
+    if (fetchArrCopy[index].likedBy.includes("chad")) {
+      fetchArrCopy[index].likedBy = fetchArrCopy[index].likedBy.filter(
+        (user) => {
+          return user !== "chad";
+        }
+      );
+      setFetchArr(fetchArrCopy);
+    } else if (!fetchArrCopy[index].likedBy.includes("chad")) {
+      fetchArrCopy[index].likedBy.push("chad");
+      setFetchArr(fetchArrCopy);
+    }
+  }
+
+  return <div>{imgArr}</div>;
 };
 
 export default LikeTestPage;
