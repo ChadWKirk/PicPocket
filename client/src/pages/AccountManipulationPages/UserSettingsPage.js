3e4ddee4a1c56d6d01eeb2b5ffa4aea129a023a4
Chad Kirk <ChadWKirk@gmail.com>
2023-01-20 17:58:58 -0600

    Made it so that when you try to upload a file that isn't png, jpeg, or jpg, it shows a faux loading icon and changes back without uploading/changing the PFP.

--- client/src/pages/AccountManipulationPages/UserSettingsPage.js
+++ client/src/pages/AccountManipulationPages/UserSettingsPage.js
@@ -80,95 +80,98 @@ const UserSettingsPage = ({ curUser, loggedIn }) => {
 
   async function uploadHandler(e) {
     e.preventDefault();
+    var fileTypeArr = ["image/png", "image/jpeg", "image/jpg"];
+    setIsDone(false);
 
     for (var i = 0; i < e.target.files.length; i++) {
       const image = e.target.files[i];
-      console.log(image);
-      if (
-        image.type != "image/png" ||
-        image.type != "image/jpeg" ||
-        image.type != "image/jpg"
-      ) {
-        image.isUploading = false;
-        image.isError = true;
+
+      if (fileTypeArr.indexOf(e.target.files[i].type.toLowerCase()) < 0) {
+        image.isUploading = true;
         setPfpToUpload(image);
-        setIsDone(true);
-        // return;
+        setTimeout(() => {
+          image.isUploading = false;
+          image.isError = true;
+          setPfpToUpload(image);
+          setIsDone(true);
+        }, 80);
+
         console.log(e.target.files[i].type);
-        // return;
-      }
-      image.isUploading = true;
-      setPfpToUpload(image);
-      targetFilesArray.push(image);
-      // setImagesToUpload((imagesToUpload) => [...imagesToUpload, image]);
-      console.log(targetFilesArray + " target files");
-
-      //to send in fetch
-      const formData = new FormData();
-      formData.append("file", image);
-      formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
-      formData.append("folder", "picpocket");
-
-      var uploadToMongoBody;
-
-      //send post request to cloudinary api upload endpoint url (have to use admin API
-      //from cloudinary for multi upload). Upload preset only allows jpg, jpeg or png files.
-      await fetch("https://api.cloudinary.com/v1_1/dtyg4ctfr/upload", {
-        method: "POST",
-        body: formData,
-      })
-        .then((result) =>
-          result.json().then((resJSON) => (uploadToMongoBody = resJSON))
-        )
-        .catch((err) => {
-          console.log(err);
-        });
-      //if file type is jpg, png or jpeg and successfully gets uploaded to cloudinary,
-      //send to mongoDB
-      if (uploadToMongoBody.public_id) {
-        //add fields to fetch response to get ready to send to MongoDB
-        uploadToMongoBody.likes = 0;
-        uploadToMongoBody.likedBy = [];
-        uploadToMongoBody.uploadedBy = curUser;
-        uploadToMongoBody.title = image.name
-          .replace(".jpg", "")
-          .replace(".png", "")
-          .replace(".jpeg", "");
-        uploadToMongoBody.description = "";
-        uploadToMongoBody.price = "$0.00";
-        uploadToMongoBody.imageType = "Photo";
-
-        //send to mongoDB
-        fetch(`http://localhost:5000/upload/pfp/${curUser}/${pfpID}`, {
-          method: "POST",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify(uploadToMongoBody),
-        })
-          .then((res) => {
-            image.isUploading = false;
-            setPfpToUpload(image);
-            setPFP(uploadToMongoBody.secure_url);
-            image.secure_url = uploadToMongoBody.secure_url;
-            image.publicId = uploadToMongoBody.public_id;
-            image.assetId = uploadToMongoBody.asset_id;
-            pfpID = uploadToMongoBody.secure_url.slice(
-              72,
-              uploadToMongoBody.secure_url.length - 4
-            );
-            // setImagesToUpload((imagesToUpload) => [...imagesToUpload]);
-            // console.log(imagesToUpload);
-          })
-          .catch((err) => {
-            console.error(err);
-            // removeImageFromUpload(image.name);
-          });
+        return;
       } else {
-        image.isError = true;
-
-        image.isUploading = false;
+        image.isUploading = true;
         setPfpToUpload(image);
-        setIsDone(true);
-        // setImageError(!imageError);
+        targetFilesArray.push(image);
+        // setImagesToUpload((imagesToUpload) => [...imagesToUpload, image]);
+        console.log(targetFilesArray + " target files");
+
+        //to send in fetch
+        const formData = new FormData();
+        formData.append("file", image);
+        formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+        formData.append("folder", "picpocket");
+
+        var uploadToMongoBody;
+
+        //send post request to cloudinary api upload endpoint url (have to use admin API
+        //from cloudinary for multi upload). Upload preset only allows jpg, jpeg or png files.
+        await fetch("https://api.cloudinary.com/v1_1/dtyg4ctfr/upload", {
+          method: "POST",
+          body: formData,
+        })
+          .then((result) =>
+            result.json().then((resJSON) => (uploadToMongoBody = resJSON))
+          )
+          .catch((err) => {
+            console.log(err);
+          });
+        //if file type is jpg, png or jpeg and successfully gets uploaded to cloudinary,
+        //send to mongoDB
+        if (uploadToMongoBody.public_id) {
+          //add fields to fetch response to get ready to send to MongoDB
+          uploadToMongoBody.likes = 0;
+          uploadToMongoBody.likedBy = [];
+          uploadToMongoBody.uploadedBy = curUser;
+          uploadToMongoBody.title = image.name
+            .replace(".jpg", "")
+            .replace(".png", "")
+            .replace(".jpeg", "");
+          uploadToMongoBody.description = "";
+          uploadToMongoBody.price = "$0.00";
+          uploadToMongoBody.imageType = "Photo";
+
+          //send to mongoDB
+          fetch(`http://localhost:5000/upload/pfp/${curUser}/${pfpID}`, {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(uploadToMongoBody),
+          })
+            .then((res) => {
+              image.isUploading = false;
+              setPfpToUpload(image);
+              setPFP(uploadToMongoBody.secure_url);
+              image.secure_url = uploadToMongoBody.secure_url;
+              image.publicId = uploadToMongoBody.public_id;
+              image.assetId = uploadToMongoBody.asset_id;
+              pfpID = uploadToMongoBody.secure_url.slice(
+                72,
+                uploadToMongoBody.secure_url.length - 4
+              );
+              // setImagesToUpload((imagesToUpload) => [...imagesToUpload]);
+              // console.log(imagesToUpload);
+            })
+            .catch((err) => {
+              console.error(err);
+              // removeImageFromUpload(image.name);
+            });
+        } else {
+          image.isError = true;
+
+          image.isUploading = false;
+          setPfpToUpload(image);
+          setIsDone(true);
+          // setImageError(!imageError);
+        }
       }
     }
   }
