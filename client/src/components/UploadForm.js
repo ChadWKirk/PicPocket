f2b19992973a3dd8d3194ab8070b445509bd0c62
Chad Kirk <ChadWKirk@gmail.com>
2023-02-21 00:25:17 -0600

    env

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -86,7 +86,7 @@ const UploadForm = ({
         uploadToMongoBody.imageType = "Photo";
 
         //send to mongoDB
-        fetch("https://picpoccket.herokuapp.com/upload", {
+        fetch("http://localhost:5000/upload", {
           method: "POST",
           headers: { "Content-type": "application/json" },
           body: JSON.stringify(uploadToMongoBody),
@@ -167,7 +167,7 @@ const UploadForm = ({
         uploadToMongoBody.imageType = "Photo";
 
         //send to mongoDB
-        fetch("https://picpoccket.herokuapp.com/upload", {
+        fetch("http://localhost:5000/upload", {
           method: "POST",
           headers: { "Content-type": "application/json" },
           body: JSON.stringify(uploadToMongoBody),
