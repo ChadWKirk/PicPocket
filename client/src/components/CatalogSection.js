8d4beafe74c3942b9d8c0050b668a7914c346326
Chad Kirk <ChadWKirk@gmail.com>
2023-03-17 22:27:29 -0500

    deleted some unused components

--- client/src/components/CatalogSection.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CatalogSection = () => {
-  return <h3>Catalog Section</h3>;
-};
-
-export default CatalogSection;
--- client/src/components/FilterBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FilterBar = () => {
-  return <h3>FilterBar</h3>;
-};
-
-export default FilterBar;
